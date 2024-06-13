0b1156d6125e3373742247b0008c51053bfa252c
tensor <864807292@qq.com>
2024-06-13 11:15:21 +0800

    start

---
+++ app/src/main/java/com/go/openfrida/MainActivity.java
@@ -0,0 +1,36 @@
+package com.go.openfrida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.go.openfrida.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'openfrida' library on application startup.
+    static {
+        System.loadLibrary("openfrida");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'openfrida' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
